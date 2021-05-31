d6d0940875d4f1fe9d7c8c36f4e4b3333e667564
1akewood <sangwoo98.kang@gmail.com>
2021-05-31 15:33:18 +0900

    1th commit: push

---
+++ greenery-ios/greenery_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  greenery_iosApp.swift
+//  greenery-ios
+//
+//  Created by Heap on 2021/05/31.
+//
+
+import SwiftUI
+
+@main
+struct greenery_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
